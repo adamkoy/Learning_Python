4a5c31db212244a05db8c004ead355166bbe7309
Adam Koyuncu <AKoyuncu@spartaglobal.com>
2019-07-15 10:08:57 +0100

    variables 1 added

---
+++ 01variables.py
@@ -0,0 +1,20 @@
+# Variables in Pthon
+
+#It's like a box
+#Has a name
+# Stores things inside
+
+my_variable = '( Hello there'
+
+# Variable with a string being printed to console
+print (my_variable)
+
+# Reassign a variable
+
+my_variable = "I'm a new content in the same variable being reassigned to my_variable" + ' one more thing'
+
+print (my_variable)
+
+#Where is the variable box?
+correct_response = 'CPU Registers'
+print('the correct response was'+ '' + correct_response)
