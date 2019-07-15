6ffbdadca81189f207c30d8e0f7c5c937e695b2d
Adam Koyuncu <AKoyuncu@spartaglobal.com>
2019-07-15 12:01:37 +0100

    Added counters

---
+++ 02data_types_strings.py
@@ -0,0 +1,67 @@
+# Numericals - Intengers, big intengers and complex numbers
+#
+# # Whole numbers
+# #print (type(my_int))
+#
+# print(13)
+# print(type(13)) # Not the same as print (type('13')) --> STRING! inside the quotes its a string
+#
+# my_int= 13
+# print (type(my_int))
+#
+# #Floats are composaite numbers
+#
+# print(13.1)
+# print(type(13.1))
+
+#operators
+# add
+num_a = 10
+num_b= 7
+print(num_a+num_b)
+
+print(num_a-num_b)
+print(num_a*10)
+print(num_a/2)
+
+#Modulis -->> check for remainders
+print(10%2)
+
+#Checking if even
+
+print((12%2)==0)
+
+print(18%3)
+
+#Logical operators
+#They evaluate two sides and ret=un a boolean
+# Check if equal ==
+a = 10
+b = 13
+print(a==b)
+print(a==10)
+
+#Checking if bigger and smaller
+print (a >b)
+print (a> 9)
+
+print (b<a)
+print(b<15)
+
+#checking bigger / smallwe or equal
+
+print(a>10)
+print(a>= 10)
+
+#Check if not something
+
+print(a != 9)
+# a is not 9, it is 10. So it returns true
+
+#Increment
+counter =10
+print(counter)
+counter +=1
+print (counter)
+counter +=1
+print(counter)
