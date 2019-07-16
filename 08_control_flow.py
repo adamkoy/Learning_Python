9670be47e044cca0ae91feb34aa4fb964433b7b1
Adam Koyuncu <AKoyuncu@spartaglobal.com>
2019-07-16 11:13:28 +0100

    if weather statement completed

--- 08_control_flow.py
+++ 08_control_flow.py
@@ -20,43 +20,53 @@
 # else:
 #     print("You are under age to vote")
 
+#
+# ####################################################################################################################
+#
+# weather ='rainy'
+# ##Strip function
+# weather = input("What is the weather like today?").lower().strip()
+#
+# if weather == 'rainy' or weather == 'stormy':
+#
+#     print('take an umbrella')
+# elif weather == "sunny":
+#     print("take your sun glasses and sunscreen")
+#
+# else:
+#     print("Go live your life")
 
-####################################################################################################################
+#
+# I want a jacket when it is rainy and stormy
+#
+# I want an umbrella only when it is rainy
+#
+# I also want a umbrella if it is foggy, because you never
+# if know.:
+#
+# Keep stuff from sunny
 
-weather ='rainy'
-##Strip function 
+
+# weather = 'sunny'
+#
+# result1 = weather =='rainy'
+# result2 = weather == 'Sunny'
+#
+#
+
+
+
 weather = input("What is the weather like today?").lower().strip()
 
-if weather == 'rainy' or weather == 'stormy':
 
-    print('take an umbrella')
-elif weather == "sunny":
-    print("take your sun glasses and sunscreen")
+if weather == 'rainy and stormy':
+  print("I want a jacket")
+
+elif weather == 'rainy':
+    print("Please take only your umbrella")
+
+elif weather== "foggy":
+    print("Take your umbrella if it is foggy ")
 
 else:
-    print("Go live your life")
-
-
-
-
-#
-# bad_weather =='rainy'
-# good_weather == 'Sunny'
-# sunny_equipment == 'Sun glasses'
-# water_protection == 'umbrella'
-#
-# weather = 'rainy'
-#
-# if bad_weather:
-#     print(f"Today, we have bad weather you need to bring your {water_protection}")
-#
-# elif good_weather:
-#     print(f"Dont worry today is perfect. Just bring your {sunny_equipment} in case")
-#
-#     weather = "Sunny"
-#
-#     if good_weather:
-#         print(f"its great weather, leave you{water_protection} at home")
-#
-#     elif bad_weather:
-#         print(f"Ow gosh, youre going to need to bring your {water_protection} and forget the{sunny_equipment}")
+    print("keep your stuff if it is sunny")
