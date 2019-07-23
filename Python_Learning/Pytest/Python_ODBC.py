0478c4ecdb76147e5244694601833588180e4291
Adam Koyuncu <AKoyuncu@spartaglobal.com>
2019-07-23 16:52:25 +0100

    Pytest OBDC environment is faulty. Created a new Environemnt called Godevops

--- Python_Learning/Pytest/Python_ODBC.py
+++ Python_Learning/Pytest/Python_ODBC.py
@@ -6,7 +6,24 @@ import pyodbc
 class NwProducts():
     def __init__(self):
         self.server = 'localhost, 1433'  # local machine - your own machine - if you dont know the name
-        self.database = 'NorthWind'
+        self.database = 'Northwind'
         self.username = 'sa'
-        self.password =  ' Passw0rd2018'
-        self.docker_Northwind = pyodbc.connect
+        self.password = 'Passw0rd2018'
+        self.docker_Northwind = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};' + 'SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
+        self.cursor = self.docker_Northwind.cursor()
+
+
+
+my_db = NwProducts()
+result = my_db.cursor.execute("select * from Products")
+# receives a Cursor when it connects which directs you - use it to read write etc
+rows = result.fetchall()
+for row in rows:
+    print(row.ProductID, row.ProductName)
+
+
+
+# create , update and alter , CRUD
+
+
+
